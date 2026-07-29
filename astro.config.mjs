214167b0606fa387878e83878a38bf5a1d5ee0c1
Basit Rahim <basitrahim66@gmail.com>
2026-07-30 00:30:00 +0500

    updaetd site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://toolhub-8hn.pages.dev',
+  site: 'http://ogtoolser.com',
   output: 'static',
   prefetch: {
     prefetchAll: true,
